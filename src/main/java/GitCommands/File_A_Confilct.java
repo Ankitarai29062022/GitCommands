4cd47c5d3afa088190a83d3f501bcd67d775ca26
Ankitarai29062022 <Ankitarai29062022@gmail.com>
2025-12-12 08:51:25 +0530

    1st commit

---
+++ src/main/java/GitCommands/File_A_Confilct.java
@@ -0,0 +1,10 @@
+package GitCommands;
+
+public class File_A_Confilct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/GitCommands/File_B_Confilct.java
@@ -0,0 +1,12 @@
+package GitCommands;
+
+public class File_B_Confilct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("B");
+
+	}
+
+}
